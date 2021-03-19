2eabbda70bc13dee45dadd1258af7eb1919a9a23
ljsoo123 <ljsoo123456@naver.com>
2021-03-19 20:27:24 +0900

    [CREATE] home 스타일

---
+++ src/components/Home/styles.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 100vh;
+`;
+
+export const InputDiv = styled.div`
+  > input {
+    width: 650px;
+    height: 50px;
+  }
+`;
+
+export const BtnDiv = styled.div`
+  > button {
+    width: 100px;
+    height: 56px;
+    border: none;
+  }
+`;
